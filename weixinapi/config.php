f506a5178fd904b9ddffe92f5f0825c88ac5d410
llz <15088256320@139.com>
2018-08-09 17:10:26 +0800

    v1

---
+++ weixinapi/config.php
@@ -0,0 +1,10 @@
+<?php
+    define('APP_ID', 'wx9c52ab6039cbf7ca');
+    define('APP_SECRET', '012bace7aaab7463829f854749f93543');
+    define('DATA_PATH', './data/');
+    /*
+     * Environment
+     */
+    define('ENV', 'sae');
+    //define('ENV', 'dev');
+    define('SAE_DATA_PATH', 'saekv://');
