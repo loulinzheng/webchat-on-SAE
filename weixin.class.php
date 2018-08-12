cf3de64d8b6eac6f92cec6a92041cfbf1668ee55
llz <15088256320@139.com>
2018-08-12 11:50:27 +0800

    v13

--- weixin.class.php
+++ weixin.class.php
@@ -250,13 +250,14 @@ function curl_post($url,$data){
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); 
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); 
     curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); 
-    //curl_setopt($curl, CURLOPT_AUTOREFERER, 1); 
+   // curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); 
+    
 	if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) {
 
     curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
 
 }
+    curl_setopt($curl, CURLOPT_AUTOREFERER, 1); 
     curl_setopt($curl, CURLOPT_POST, 1); 
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data); 
     curl_setopt($curl, CURLOPT_TIMEOUT, 30); 
