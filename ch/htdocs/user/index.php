620aac5fb6788cbe4410286e72fe7b2986025b18
llz <15088256320@139.com>
2018-08-16 19:41:51 +0800

    v14

--- ch/htdocs/user/index.php
+++ ch/htdocs/user/index.php
@@ -1,6 +1,6 @@
 <?php
 //if ( ! defined('IN_APP')) exit('No direct script access allowed');
-require libfile("../../lib/weixin.class.php");
+require libfile("./../../lib/weixin.class.php");
 
 
 $ret = weixin::getUserList();
