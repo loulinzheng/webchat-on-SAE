655905adb3f165742d1e2854ca2d05cef51a8b18
llz <15088256320@139.com>
2018-08-21 17:06:08 +0800

    v15

--- oauth1.php
+++ oauth1.php
@@ -47,29 +47,29 @@ return $data;
 ?>
 <div class="fieldcontain">
  <lable for="openid">OpenID</lable>
- <input name="openid" id="openid" value="<?php echo $userinfo['openid']; ?> type=text">
+ <input name="openid" id="openid" value="<?php echo $userinfo['openid']; ?>" type="text">
 </div>
 <div class="fieldcontain">
  <lable for="nickname">昵称</lable>
- <input name="nickname" id="nickname" value="<?php echo $userinfo['nickname']; ?> type=text">
+ <input name="nickname" id="nickname" value="<?php echo $userinfo['nickname']; ?>" type="text">
 </div>
 <div class="fieldcontain">
  <lable for="sex">性别</lable>
- <input name="sex" id="sex" value="<?php echo $userinfo['sex']; ?> type=text">
+ <input name="sex" id="sex" value="<?php echo $userinfo['sex']; ?>" type="text">
 </div>
 <div class="fieldcontain">
  <lable for="country">国家</lable>
- <input name="country" id="country" value="<?php echo $userinfo["country"]; ?> type=text">
+ <input name="country" id="country" value="<?php echo $userinfo["country"]; ?>" type="text">
 </div>
 <div class="fieldcontain">
  <lable for="province">省份</lable>
- <input name="province" id="province" value="<?php echo $userinfo["province"]; ?> type=text">
+ <input name="province" id="province" value="<?php echo $userinfo["province"]; ?>" type="text">
 </div>
 <div class="fieldcontain">
  <lable for="city">城市</lable>
- <input name="city" id="city" value="<?php echo $userinfo["city"]; ?> type=text">
+ <input name="city" id="city" value="<?php echo $userinfo["city"]; ?>" type="text">
 </div>
 <div class="fieldcontain">
  <lable for="privilege">特权</lable>
- <input name="privilege" id="privilege" value="<?php echo $userinfo["privilege"]; ?> type=text">
+ <input name="privilege" id="privilege" value="<?php echo $userinfo["privilege"]; ?>" type="text">
 </div>
